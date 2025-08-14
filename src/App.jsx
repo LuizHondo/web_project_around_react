c98db605d1be4151c106526c64cfb62d79e2392b
Luiz Paulo Hondo <lpmoh2@hotmail.com>
2025-08-14 12:00:31 -0300

    App.jsx import removed

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,6 @@ import { use, useState } from 'react'
 import Header from "./components/Header/Header.jsx"
 import Main from "./components/Main/Main.jsx"
 import Footer from "./components/Footer/Footer.jsx"
-import Popup from "./components/Popup/Popup.jsx"
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-
 
 export default function App() {
 
